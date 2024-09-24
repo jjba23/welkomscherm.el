dc34b9437971784c97cccb62bb5d9306c3c85a93
Josep Bigorra <jjbigorra@gmail.com>
2024-09-24 23:01:19 +0200

    nice!

--- welkomscherm.el
+++ welkomscherm.el
@@ -8,33 +8,56 @@
 
 
 (defun welkomscherm-insert-muted (x)
-  (insert (propertize x 'face 'welkomscherm-muted-face))
-  )
+  (insert (propertize x 'face 'welkomscherm-muted-face)))
+
+(defun welkomscherm-insert-new-line () (insert "\n"))
 
 (defcustom welkomscherm-buffer-name "*welkomscherm*"
   ""
   :type 'string
   :group 'welkomscherm)
 
-(defvar welkomscherm-bookmarks '("~/Ontwikkeling/Persoonlijk/dotfiles/users/joe/emacs/init.el"
-                                 "~/Ontwikkeling/Persoonlijk/welkomscherm.el/welkomscherm.el")
-  ""  )
+(defcustom welkomscherm-top-title '("While any editor can save your files" "only Emacs can save your soul !")
+  ""
+  :type '(list string)
+  :group 'welkomscherm)
+
+(defcustom welkomscherm-bookmarks '("~/.emacs.d/init.el"
+                                    "~/.ssh/config")
+  ""
+  :type '(list string)
+  :group 'welkomscherm)
+
+(defun welkomscherm-insert ()
+  ""
+  (dolist (x welkomscherm-top-title)
+    (welkomscherm-insert-muted x)
+    (center-line)
+    (welkomscherm-insert-new-line)
+    )
+  
+  (center-line)
+  (welkomscherm-insert-new-line)
+  (welkomscherm-insert-new-line)
+  (dolist (x welkomscherm-bookmarks)
+    (insert-button x
+                   'my-file-path x
+                   'action (lambda(btn) (find-file (button-get btn 'my-file-path))))
+    (center-line)
+    (welkomscherm-insert-new-line)
+    )
+  (welkomscherm-insert-new-line)
+  )
 
 (defun welkomscherm ()
   (interactive)
-  (kill-buffer welkomscherm-buffer-name)
+  (when (buffer-live-p (get-buffer welkomscherm-buffer-name)) (kill-buffer welkomscherm-buffer-name))
+  
   (let ((buf (get-buffer-create welkomscherm-buffer-name)))
     (with-current-buffer welkomscherm-buffer-name
       (read-only-mode -1)
-      (welkomscherm-insert-muted welkomscherm-buffer-name)
-      (center-line)
-      (insert "\n")
-      (dolist (el welkomscherm-bookmarks)
-        (print el)
-        (insert-button el '(:action '(find-file el)))
-        (insert "\n")
-        )
-      (insert "\n")
+      (setq-local line-spacing 4)
+      (welkomscherm-insert)
       (read-only-mode 1)
       )
     (switch-to-buffer welkomscherm-buffer-name)
