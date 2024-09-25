c708441a99dac73421652098a23481af2b214d30
Josep Bigorra <jjbigorra@gmail.com>
2024-09-25 08:34:23 +0200

    commentary

--- welkomscherm.el
+++ welkomscherm.el
@@ -1,3 +1,34 @@
+;;; welkomscherm.el --- Proportionally sized faces -*- lexical-binding: t -*-
+
+;; Copyright (C) 2024 Josep Bigorra
+
+;; Version: 0.1.1
+;; Author: Josep Bigorra <jjbigorra@gmail.com>
+;; Maintainer: Josep Bigorra <jjbigorra@gmail.com>
+;; URL: https://github.com/jjba23/tekengrootte.el
+;; Keywords: faces
+;; Package: tekengrootte
+;; Package-Requires: ((emacs "25.1"))
+
+
+;; welkomscherm is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; welkomscherm is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; TODO
+
+;;; Code:
 
 (defgroup welkomscherm ()
   ""
@@ -50,17 +81,22 @@
   )
 
 (defun welkomscherm ()
+  "Start a new Welkomscherm."
   (interactive)
   (when (buffer-live-p (get-buffer welkomscherm-buffer-name)) (kill-buffer welkomscherm-buffer-name))
   
   (let ((buf (get-buffer-create welkomscherm-buffer-name)))
-    (with-current-buffer welkomscherm-buffer-name
+    (with-current-buffer buf
       (read-only-mode -1)
       (setq-local line-spacing 4)
       (welkomscherm-insert)
       (read-only-mode 1)
       )
-    (switch-to-buffer welkomscherm-buffer-name)
+    (switch-to-buffer buf)
     )
   )
 
+
+(provide 'welkomscherm)
+
+;;; welkomscherm.el ends here
