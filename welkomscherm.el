134e9b85571c82d605de4ee06f74b901d99a55e9
Josep Bigorra <jjbigorra@gmail.com>
2024-09-25 12:39:31 +0200

    dashboard

--- welkomscherm.el
+++ welkomscherm.el
@@ -8,7 +8,7 @@
 ;; URL: https://github.com/jjba23/tekengrootte.el
 ;; Keywords: faces
 ;; Package: tekengrootte
-;; Package-Requires: ((emacs "25.1"))
+;; Package-Requires: ((emacs "25.1") (page-break-lines "0.15"))
 
 
 ;; welkomscherm is free software: you can redistribute it and/or modify
@@ -30,6 +30,8 @@
 
 ;;; Code:
 
+(require 'page-break-lines)
+
 (defgroup welkomscherm ()
   ""
   :group 'tools)
@@ -39,135 +41,157 @@
 
 
 (defun welkomscherm-insert-muted (x)
-  (insert (propertize x 'face 'welkomscherm-muted-face)))
+  (insert (propertize x 'face 'welkomscherm-muted-face))
+  (when welkomscherm-centered (center-line)))
 
 (defun welkomscherm-insert-new-line () (insert "\n"))
 
-(defun welkomscherm-insert-spacer () (insert "   "))
+(defun welkomscherm-insert-form-feed () (insert "\f"))
+
+(defun welkomscherm-insert-spacer () (insert "  "))
 
 (defcustom welkomscherm-buffer-name "*welkomscherm*"
   ""
   :type 'string
   :group 'welkomscherm)
 
-(defcustom welkomscherm-top-title '("While any editor can save your files" "only Emacs can save your soul !")
-  ""
+(defcustom welkomscherm-top-title
+  '("While any editor can save your files" "only Emacs can save your soul !")
+  "Text lines to show as top title banner."
   :type '(list string)
   :group 'welkomscherm)
 
 
-(defcustom welkomscherm-bookmarks-top-name "bookmarks: "
-  "" :type 'string :group 'welkomscherm)
+(defcustom welkomscherm-bookmarks-personal-name "bookmarks: "
+  "Title to show in personal bookmark section."
+  :type 'string :group 'welkomscherm)
 
-(defcustom welkomscherm-bookmarks-middle-name "actions: "
-  "" :type 'string :group 'welkomscherm)
+(defcustom welkomscherm-actions-name "actions: "
+  "Title to show for action buttons."
+  :type 'string :group 'welkomscherm)
 
-(defcustom welkomscherm-bookmarks-bottom-name "work: "
-  "" :type 'string :group 'welkomscherm)
+(defcustom welkomscherm-bookmarks-work-name "work: "
+  "Title to show for work bookmark section."
+  :type 'string :group 'welkomscherm)
 
-(defcustom welkomscherm-bookmarks-top
+(defcustom welkomscherm-use-form-feed nil
+  "" :type 'boolean :group 'welkomscherm)
+
+(defcustom welkomscherm-centered t
+  "" :type 'boolean :group 'welkomscherm)
+
+(defcustom welkomscherm-use-section-title t
+  "" :type 'boolean :group 'welkomscherm)
+
+(defcustom welkomscherm-bookmarks-personal
   '((("row0c0" . "~/Dev/row0c0")
-     ("row0c1" . "~/Downloads/some-file.pdf")
-     )
-    (("row1c0" . "~/Documents/row1c0")
-     ))
-  "Bookmark alias and path pairs shown up top."
+     ("row0c1" . "~/Downloads/some-file.pdf"))
+    (("row1c0" . "~/Documents/row1c0")))
+  "Bookmark alias and path pairs for personal."
   :type 'alist
   :group 'welkomscherm)
 
-
-(defcustom welkomscherm-bookmarks-bottom
+(defcustom welkomscherm-bookmarks-work
   '((("work-project-1" . "~/Work/work-project-1/")
-     ("wiki-work" . "~/Work/wiki-work/"))    
-    )
-  "Bookmark alias and path pairs shown down below."
+     ("wiki-work" . "~/Work/wiki-work/")))
+  "Bookmark alias and path pairs for work."
   :type 'alist
   :group 'welkomscherm)
 
-(defcustom welkomscherm-middle-buttons
+(defcustom welkomscherm-buttons-actions
   '((("*scratch*" . (lambda(btn) (switch-to-buffer "*scratch*")))
      ("*Messages*" . (lambda(btn) (switch-to-buffer "*Messages*")))
      ("re-render me" . (lambda(btn) (welkomscherm)))
-     )
-    
-    )
+     ))
   "Buttons with desired actions in the middle."
   :type 'alist
   :group 'welkomscherm)
 
 (defun welkomscherm-on-button-click (btn)
-  (let (
-        (welkomscherm-path (button-get btn 'welkomscherm-path))
-        )
-    
+  ""
+  (let ((welkomscherm-path (button-get btn 'welkomscherm-path)))
     (find-file welkomscherm-path)))
 
-(defun welkomscherm-insert ()
-  "Render a new Welkomscherm."
-
-  (dolist (x welkomscherm-top-title)
-    (welkomscherm-insert-muted x)
-    (center-line)
-    (welkomscherm-insert-new-line))
-  
-  (welkomscherm-insert-new-line)
-  (welkomscherm-insert-new-line)
-
-  (welkomscherm-insert-muted welkomscherm-bookmarks-top-name)
-  (welkomscherm-insert-new-line)
-
-  (dolist (row welkomscherm-bookmarks-top)
+(defun welkomscherm-insert-bookmark-rows (rows)
+  "Render ROWS of bookmarks."
+  (dolist (row rows)
     (dolist (x row)
       (insert-button (car x)
                      'welkomscherm-alias (car x)
                      'welkomscherm-path (cdr x)
                      'action 'welkomscherm-on-button-click)
       (welkomscherm-insert-spacer))
-    (welkomscherm-insert-new-line))
-  
+    (welkomscherm-maybe-centered)
+    (welkomscherm-insert-new-line)))
 
+(defun welkomscherm-maybe-form-feed ()
+  ""
+  (when welkomscherm-use-form-feed (welkomscherm-insert-form-feed)))
+
+(defun welkomscherm-maybe-centered ()
+  ""
+  (when welkomscherm-centered (center-line)))
+
+(defun welkomscherm-insert ()
+  "Render a new Welkomscherm."
+  
+  (dolist (x welkomscherm-top-title)
+    (welkomscherm-insert-muted x)
+    (welkomscherm-maybe-centered)
+    (welkomscherm-insert-new-line))
   (welkomscherm-insert-new-line)
-  (welkomscherm-insert-muted welkomscherm-bookmarks-middle-name)
+
+  (welkomscherm-maybe-form-feed)
+  (when welkomscherm-use-section-title
+    (welkomscherm-insert-muted welkomscherm-bookmarks-personal-name)) 
   (welkomscherm-insert-new-line)
-  (dolist (row welkomscherm-middle-buttons)
+  (welkomscherm-insert-bookmark-rows welkomscherm-bookmarks-top)
+
+  (welkomscherm-maybe-form-feed)
+  (welkomscherm-insert-new-line)
+  (when welkomscherm-use-section-title
+    (welkomscherm-insert-muted welkomscherm-actions-name))
+  (welkomscherm-insert-new-line)
+  
+  (dolist (row welkomscherm-buttons-actions)
     (dolist (x row)
       (insert-button (car x)
                      'action (cdr x))
       (welkomscherm-insert-spacer))
+    (welkomscherm-maybe-centered)
     (welkomscherm-insert-new-line))
 
 
+  (welkomscherm-maybe-form-feed)
+  (welkomscherm-insert-new-line)
+  (when welkomscherm-use-section-title
+    (welkomscherm-insert-muted welkomscherm-bookmarks-work-name))
 
   (welkomscherm-insert-new-line)
-  (welkomscherm-insert-muted welkomscherm-bookmarks-bottom-name)
-  (welkomscherm-insert-new-line)
-  (dolist (row welkomscherm-bookmarks-bottom)
-    (dolist (x row)
-      (insert-button (car x)
-                     'welkomscherm-alias (car x)
-                     'welkomscherm-path (cdr x)
-                     'action 'welkomscherm-on-button-click)
-      (welkomscherm-insert-spacer))
-    (welkomscherm-insert-new-line))
-  
-  
-  (welkomscherm-insert-new-line))
+  (welkomscherm-insert-bookmark-rows welkomscherm-bookmarks-work)
+  (welkomscherm-maybe-form-feed))
 
 (defun welkomscherm ()
   "Start a new Welkomscherm killing any live previous instances."
   (interactive)
-  (when (buffer-live-p (get-buffer welkomscherm-buffer-name)) (kill-buffer welkomscherm-buffer-name))
+  
+  (when (buffer-live-p (get-buffer welkomscherm-buffer-name))
+    (kill-buffer welkomscherm-buffer-name))
   
   (let ((buf (get-buffer-create welkomscherm-buffer-name)))
     (with-current-buffer buf
       (read-only-mode -1)
       (setq line-spacing 12)
       (welkomscherm-insert)
-      (read-only-mode 1))
-    (switch-to-buffer buf)))
+      (read-only-mode 1)
+      (page-break-lines-mode)
+      )
+    (switch-to-buffer buf)
+    (setq fringe-indicator-alist '((truncation nil)))))
 
 
 (provide 'welkomscherm)
 
 ;;; welkomscherm.el ends here
+
 
